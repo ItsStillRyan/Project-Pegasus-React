6f179a2135decdc89f77f9b0152bd0bb94e3a05b
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-03-06 09:52:30 +0000

    placeholder footer added, colorscheme matched

--- project-pegasus/src/App.js
+++ project-pegasus/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavbarMain from './navbar'
 import Main from './main'
+import MainFooter from './footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar'
 import './main.css'
@@ -8,17 +9,17 @@ import './main.css'
 
 function App() {
     return (
-            <div className="bodyT">
-                <div className="App">
-                    <NavbarMain />
-                    <header>
-                        <Main />
-                    </header>
-                    <footer>
-                        
-                    </footer>
-                </div>
+        <div className="bodyT">
+            <div className="App">
+                <NavbarMain />
+                <header>
+                    <Main />
+                </header>
+                <footer>
+                    <MainFooter />
+                </footer>
             </div>
+        </div>
     )
 }
 
