14c4f942cd8764dba084b78eebd1242b1e6c815d
ItsStillRyan <Ryan_Tan_1998@hotmail.sg>
2021-03-08 08:45:19 +0000

    added axios framework into project

--- project-pegasus/src/pages/Category-cluster/galaxies.js
+++ project-pegasus/src/pages/Category-cluster/galaxies.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 //Bootstraps
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -10,74 +11,113 @@ import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, 
 import '../../main.css'
 //image imports
 import testImg from '../../images/categories-img/galaxies.png'
+//API
+const baseURL = "https://3000-black-marmoset-zifzl6nb.ws-us03.gitpod.io/"
 
 
+export default class Galaxies extends React.Component {
 
-const Galaxies = () => {
-    return (
-        <React.Fragment>
-            <Container>
-                <Row>
-                    <Col className="cateTitle">
-                        <p>Galaxies</p>
+    async componentDidMount() {
+        let response = await axios.get(baseURL + "/show")
+        this.setState({
+            small_details: response.data
+        })
+    }
+
+    renderIndiviCard = () => {
+        let accumulator = []
+        for (let galax_post of this.state.small_details) {
+            accumulator.push(
+                <div key={galax_post._id}>
+                    <Col>
+                        <MDBCard className="cateCardStyle">
+                            <MDBCardImage className="img-fluid" src="https://i.redd.it/qfpg400call61.png" waves />
+                            <MDBCardBody>
+                                <MDBCardTitle>{galax_post[0].user_uploads.content.title}</MDBCardTitle>
+                                
+                                <MDBCardText>{galax_post[0].user_uploads.details.name}</MDBCardText>
+                                <MDBBtn href="#">Engage</MDBBtn>
+                            </MDBCardBody>
+                        </MDBCard>
                     </Col>
-                </Row>
-                <Row>
-                    <Col className="cateSub">
-                        <p>
-                            gal•ax•y găl′ək-sē
+                </div>
+            )
+        }
+        return accumulator
+    }
+
+
+
+
+
+
+
+
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <Container>
+                    <Row>
+                        <Col className="cateTitle">
+                            <p>Galaxies</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="cateSub">
+                            <p>
+                                gal•ax•y găl′ək-sē
                         </p>
-                        <p>Any of numerous large-scale aggregates of stars, gas, and dust that constitute the universe, containing an average of 100 billion (10<sup>11</sup>) solar masses and ranging in diameter from 1,500 to 300,000 light-years.</p>
-                    </Col>
-                </Row>
-            </Container>
+                            <p>Any of numerous large-scale aggregates of stars, gas, and dust that constitute the universe, containing an average of 100 billion (10<sup>11</sup>) solar masses and ranging in diameter from 1,500 to 300,000 light-years.</p>
+                        </Col>
+                    </Row>
+                </Container>
 
-            {/* PAST HERE IS THE ACTUAL CONTENT */}
-            <div className="cateCardContain">
-                <Row>
-                    <Col>
-                        <MDBCard className="cateCardStyle">
-                            <MDBCardImage className="img-fluid" src="https://i.redd.it/qfpg400call61.png" waves />
-                            <MDBCardBody>
-                                <MDBCardTitle>M42 - Orion Nebula</MDBCardTitle>
-                                <MDBCardText>
-                                    by u/_snowtrooper_
+                {/* PAST HERE IS THE ACTUAL CONTENT */}
+                <div className="cateCardContain">
+                    <Row className="cateIndiviRow">
+                        {this.renderIndiviCard}
+                        {/* <Col>
+                            <MDBCard className="cateCardStyle">
+                                <MDBCardImage className="img-fluid" src="https://i.redd.it/qfpg400call61.png" waves />
+                                <MDBCardBody>
+                                    <MDBCardTitle>M42 - Orion Nebula</MDBCardTitle>
+                                    <MDBCardText>
+                                        by u/_snowtrooper_
                                     </MDBCardText>
-                                <MDBBtn href="#">Engage</MDBBtn>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </Col>
-                    <Col>
-                        <MDBCard className="cateCardStyle">
-                            <MDBCardImage className="img-fluid" src="https://i.redd.it/qfpg400call61.png" waves />
-                            <MDBCardBody>
-                                <MDBCardTitle>M42 - Orion Nebula</MDBCardTitle>
-                                <MDBCardText>
-                                    by u/_snowtrooper_
+                                    <MDBBtn href="#">Engage</MDBBtn>
+                                </MDBCardBody>
+                            </MDBCard>
+                        </Col>
+                        <Col>
+                            <MDBCard className="cateCardStyle">
+                                <MDBCardImage className="img-fluid" src="https://i.redd.it/qfpg400call61.png" waves />
+                                <MDBCardBody>
+                                    <MDBCardTitle>M42 - Orion Nebula</MDBCardTitle>
+                                    <MDBCardText>
+                                        by u/_snowtrooper_
                                     </MDBCardText>
-                                <MDBBtn href="#">Engage</MDBBtn>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </Col>
-                    <Col>
-                        <MDBCard className="cateCardStyle">
-                            <MDBCardImage className="img-fluid" src="https://i.redd.it/qfpg400call61.png" waves />
-                            <MDBCardBody>
-                                <MDBCardTitle>M42 - Orion Nebula</MDBCardTitle>
-                                <MDBCardText>
-                                    by u/_snowtrooper_
+                                    <MDBBtn href="#">Engage</MDBBtn>
+                                </MDBCardBody>
+                            </MDBCard>
+                        </Col>
+                        <Col>
+                            <MDBCard className="cateCardStyle">
+                                <MDBCardImage className="img-fluid" src="https://i.redd.it/qfpg400call61.png" waves />
+                                <MDBCardBody>
+                                    <MDBCardTitle>M42 - Orion Nebula</MDBCardTitle>
+                                    <MDBCardText>
+                                        by u/_snowtrooper_
                                     </MDBCardText>
-                                <MDBBtn href="#">Engage</MDBBtn>
-                            </MDBCardBody>
-                        </MDBCard>
-                    </Col>
-                </Row>
+                                    <MDBBtn href="#">Engage</MDBBtn>
+                                </MDBCardBody>
+                            </MDBCard>
+                        </Col> */}
+                    </Row>
+                </div>
+            </React.Fragment>
 
-
-
-            </div>
-        </React.Fragment>
-
-    )
+        )
+    }
 }
-export default Galaxies
